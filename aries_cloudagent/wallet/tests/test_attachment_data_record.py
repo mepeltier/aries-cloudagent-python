62981100cd5eddc00298eb3cec0b2305b87ce76d
Char Howland <char@indicio.tech>
2022-09-02 17:52:49 -0600

    test: save_attachments method
    Signed-off-by: Char Howland <char@indicio.tech>

--- aries_cloudagent/wallet/tests/test_attachment_data_record.py
+++ aries_cloudagent/wallet/tests/test_attachment_data_record.py
@@ -1,8 +1,11 @@
 import pytest
 
-from ..models.attachment_data_record import AttachmentDataRecord
 from ...messaging.decorators.attach_decorator import AttachDecorator
+from ...messaging.responder import BaseResponder, MockResponder
 from ...protocols.issue_credential.v2_0.messages.inner.supplement import Supplement
+from ...core.in_memory import InMemoryProfile
+from ...core.event_bus import EventBus
+from ..models.attachment_data_record import AttachmentDataRecord
 
 
 @pytest.fixture
@@ -34,6 +37,26 @@ def create_attachment():
     return _create_attachment
 
 
+@pytest.fixture
+def event_bus():
+    """Event bus fixture."""
+    yield EventBus()
+
+
+@pytest.fixture
+def mock_responder():
+    """Mock responder fixture."""
+    yield MockResponder()
+
+
+@pytest.fixture
+def profile(event_bus, mock_responder):
+    """Profile fixture."""
+    yield InMemoryProfile.test_profile(
+        bind={EventBus: event_bus, BaseResponder: mock_responder}
+    )
+
+
 def test_attachment_lookup(create_attachment):
 
     attachments = [create_attachment(1), create_attachment(2)]
@@ -52,8 +75,28 @@ def test_match_by_attachment_id(create_supplement, create_attachment):
 
     supplements = [create_supplement(1), create_supplement(2)]
     attachments = [create_attachment(1), create_attachment(2)]
-    result = AttachmentDataRecord.match_by_attachment_id(supplements, attachments)
+    result = AttachmentDataRecord.match_by_attachment_id(
+        supplements, attachments, cred_id="test_cred_id", attribute="test_attribute"
+    )
 
     for record in result:
         assert type(record) == AttachmentDataRecord
         assert record.attachment_id == record.supplement.id
+
+
+@pytest.mark.asyncio
+async def test_save_attachments(create_supplement, create_attachment, profile):
+
+    async with profile.session() as session:
+        supplements = [create_supplement(1), create_supplement(2)]
+        attachments = [create_attachment(1), create_attachment(2)]
+        result = await AttachmentDataRecord.save_attachments(
+            session,
+            supplements,
+            attachments,
+            cred_id="test_cred_id",
+            attribute="test_attribute",
+        )
+
+        assert type(result) == list
+        assert len(result) == 2
